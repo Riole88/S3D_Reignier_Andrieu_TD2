e3de984716d6d986c287a6569e60f7e928808ab4
Paul5400 <160632943+Paul5400@users.noreply.github.com>
2024-09-25 11:48:41 +0200

    ajout des méthodes dans GroupeEtudiant

--- src/GroupeEtudiant.java
+++ src/GroupeEtudiant.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.List;
+
 public class GroupeEtudiant {
     ArrayList<Etudiant> groupeE ;
     Formation formation;
@@ -7,15 +9,31 @@ public class GroupeEtudiant {
         this.groupeE = new ArrayList<Etudiant>();
         this.formation=f1;
     }
-    public void ajouterE(Etudiant e){
+    public void ajouterE(Etudiant e)  {
         if (e.getFormation().getIdt().equals(formation.getIdt()) ){
             groupeE.add(e);
 
+        } else {
+            System.out.println("l'étudiant n'est pas dans la formation");
         }
+
 
     }
     public void supprimerE(Etudiant e){
         groupeE.remove(e);
     }
 
+
+
+    public List<Etudiant> getEtudiant(){
+        return groupeE;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupeEtudiant{" +
+                "groupeE=" + groupeE +
+                ", formation=" + formation +
+                '}';
+    }
 }
